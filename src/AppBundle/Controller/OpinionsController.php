9f7f8b55e56d15cb715a6d4e7f9bad1dedddb91d
staho <stacho688@gmail.com>
2017-10-24 08:02:16 +0200

    Added controllers and views

---
+++ src/AppBundle/Controller/OpinionsController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: staho
+ * Date: 24.10.2017
+ * Time: 00:35
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class OpinionsController extends Controller
+{
+    /**
+     * @Route("/opinions/", name="opinions")
+     */
+
+    public function showAction(){
+        return $this->render('base.html.twig');
+    }
+
+}
---
+++ src/AppBundle/Controller/YourPetController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: staho
+ * Date: 24.10.2017
+ * Time: 00:30
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class YourPetController extends Controller
+{
+    /**
+     * @Route("/yourpet", name="yourpet")
+     */
+
+    public function showAction(){
+        return $this->render('base.html.twig');
+    }
+
+}
